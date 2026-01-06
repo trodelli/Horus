63132e5be416ba3a506a5cc02fc2d5dcc9b06825
thomas <trrodelli@icloud.com>
2026-01-06 08:36:27 +0100

    Initial Commit

---
+++ Horus/HorusApp.swift
@@ -0,0 +1,17 @@
+//
+//  HorusApp.swift
+//  Horus
+//
+//  Created by Thomas Rodelli on 06/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct HorusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HorusTests/HorusTests.swift
@@ -0,0 +1,17 @@
+//
+//  HorusTests.swift
+//  HorusTests
+//
+//  Created by Thomas Rodelli on 06/01/2026.
+//
+
+import Testing
+@testable import Horus
+
+struct HorusTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
